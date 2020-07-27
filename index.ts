2dc11bd4528e88d32876575e96f27a5fb58bb911
Elyx0 <elyx00@gmail.com>
2020-07-27 03:29:17 -0400

    WIP 08-entity-component-system

--- index.ts
+++ index.ts
@@ -1,19 +1,22 @@
-import express from 'express';
-import serveIndex from 'serve-index';
-import path from 'path';
-import cors from 'cors';
-import { createServer } from 'http';
-import { Server, LobbyRoom, RelayRoom } from 'colyseus';
-import { monitor } from '@colyseus/monitor';
+import { LobbyRoom, RelayRoom, Server } from "colyseus";
 
+import { AuthRoom } from "./rooms/03-auth";
 // Import demo room handlers
 import { ChatRoom } from "./rooms/01-chat-room";
+import { CustomLobbyRoom } from "./rooms/07-custom-lobby-room";
+import { EcsDemoRoom } from "./rooms/08-ecs-room";
+import { ReconnectionRoom } from "./rooms/04-reconnection";
 import { StateHandlerRoom } from "./rooms/02-state-handler";
-import { AuthRoom } from "./rooms/03-auth";
-import { ReconnectionRoom } from './rooms/04-reconnection';
-import { CustomLobbyRoom } from './rooms/07-custom-lobby-room';
+import cors from "cors";
+import { createServer } from "http";
+import express from "express";
+import { monitor } from "@colyseus/monitor";
+import path from "path";
+import serveIndex from "serve-index";
 
-const port = Number(process.env.PORT || 2567) + Number(process.env.NODE_APP_INSTANCE || 0);
+const port =
+    Number(process.env.PORT || 2567) +
+    Number(process.env.NODE_APP_INSTANCE || 0);
 const app = express();
 
 app.use(cors());
@@ -21,51 +24,51 @@ app.use(express.json());
 
 // Attach WebSocket Server on HTTP Server.
 const gameServer = new Server({
-  server: createServer(app),
-  express: app,
-  pingInterval: 0,
+    server: createServer(app),
+    express: app,
+    pingInterval: 0,
 });
 
 // Define "lobby" room
 gameServer.define("lobby", LobbyRoom);
 
 // Define "relay" room
-gameServer.define("relay", RelayRoom, { maxClients: 4 })
+gameServer
+    .define("relay", RelayRoom, { maxClients: 4 })
     .enableRealtimeListing();
 
 // Define "chat" room
-gameServer.define("chat", ChatRoom)
-    .enableRealtimeListing();
+gameServer.define("chat", ChatRoom).enableRealtimeListing();
 
 // Register ChatRoom with initial options, as "chat_with_options"
 // onInit(options) will receive client join options + options registered here.
 gameServer.define("chat_with_options", ChatRoom, {
-    custom_options: "you can use me on Room#onCreate"
+    custom_options: "you can use me on Room#onCreate",
 });
 
 // Define "state_handler" room
-gameServer.define("state_handler", StateHandlerRoom)
-    .enableRealtimeListing();
+gameServer.define("state_handler", StateHandlerRoom).enableRealtimeListing();
 
 // Define "auth" room
-gameServer.define("auth", AuthRoom)
-    .enableRealtimeListing();
+gameServer.define("auth", AuthRoom).enableRealtimeListing();
 
 // Define "reconnection" room
-gameServer.define("reconnection", ReconnectionRoom)
-    .enableRealtimeListing();
+gameServer.define("reconnection", ReconnectionRoom).enableRealtimeListing();
 
 // Define "custom_lobby" room
 gameServer.define("custom_lobby", CustomLobbyRoom);
 
-app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
-app.use('/', express.static(path.join(__dirname, "static")));
+// Define "custom_lobby" room
+gameServer.define("ecs_demo", EcsDemoRoom);
+
+app.use("/", serveIndex(path.join(__dirname, "static"), { icons: true }));
+app.use("/", express.static(path.join(__dirname, "static")));
 
 // (optional) attach web monitoring panel
-app.use('/colyseus', monitor());
+app.use("/colyseus", monitor());
 
-gameServer.onShutdown(function(){
-  console.log(`game server is going down.`);
+gameServer.onShutdown(function () {
+    console.log(`game server is going down.`);
 });
 
 gameServer.listen(port);
@@ -75,4 +78,4 @@ gameServer.listen(port);
 //   process.exit(1);
 // });
 
-console.log(`Listening on http://localhost:${ port }`);
+console.log(`Listening on http://localhost:${port}`);
---
+++ rooms/08-ecs-room.ts
@@ -0,0 +1,156 @@
+import { Client, Room } from "colyseus";
+import {
+    Component,
+    Entity,
+    System,
+    TagComponent,
+    World,
+} from "@colyseus/ecs/src";
+import { Schema, type } from "@colyseus/schema";
+
+const SPEED_MULTIPLIER = 0.3;
+const SHAPE_SIZE = 50;
+const SHAPE_HALF_SIZE = SHAPE_SIZE / 2;
+
+// Initialize canvas
+let window = { innerWidth: 800, innerHeight: 600 };
+let canvas = { width: 800, height: 600 };
+let canvasWidth = (canvas.width = window.innerWidth);
+let canvasHeight = (canvas.height = window.innerHeight);
+
+// Player information component
+class PlayerInfo extends Component<any> {
+    @type("string") sessionId: string;
+}
+
+// Velocity component
+class Velocity extends Component<any> {
+    @type("number") x: number;
+    @type("number") y: number;
+}
+
+// Position component
+class Position extends Component<any> {
+    @type("number") x: number;
+    @type("number") y: number;
+}
+
+class Shape extends Component<any> {
+    @type("string") primitive: string = "box";
+}
+
+class Size extends Component<any> {
+    @type("uint8") radius: number;
+}
+
+// Renderable component
+class Renderable extends TagComponent {}
+
+// MovableSystem
+class MovableSystem extends System {
+    static queries = {
+        moving: {
+            components: [Velocity, Position],
+        },
+    };
+    // This method will get called on every frame by default
+    execute(delta, time) {
+        // Iterate through all the entities on the query
+        this.queries.moving.results.forEach((entity) => {
+            var velocity = entity.getComponent(Velocity);
+            var position = entity.getMutableComponent(Position);
+            position.x += velocity.x * delta;
+            position.y += velocity.y * delta;
+
+            if (position.x > canvasWidth + SHAPE_HALF_SIZE)
+                position.x = -SHAPE_HALF_SIZE;
+            if (position.x < -SHAPE_HALF_SIZE)
+                position.x = canvasWidth + SHAPE_HALF_SIZE;
+            if (position.y > canvasHeight + SHAPE_HALF_SIZE)
+                position.y = -SHAPE_HALF_SIZE;
+            if (position.y < -SHAPE_HALF_SIZE)
+                position.y = canvasHeight + SHAPE_HALF_SIZE;
+        });
+    }
+}
+
+export class EcsDemoRoom extends Room {
+    world: World; // Has a world.state with the entities array
+    sessionIdToEntityMap = new Map<string, Entity>();
+
+    onCreate() {
+        const world = new World();
+        this.world = world;
+        world
+            .registerComponent(PlayerInfo)
+            .registerComponent(Velocity)
+            .registerComponent(Size)
+            .registerComponent(Position)
+            .registerComponent(Shape)
+            .registerComponent(Renderable)
+            .registerSystem(MovableSystem);
+        this.setState(world.state);
+    }
+
+    onJoin(client: Client, options) {
+        const entity = this.world
+            .createEntity()
+            .addComponent(PlayerInfo, { sessionId: client.sessionId })
+            .addComponent(Velocity, getRandomVelocity())
+            .addComponent(Shape, getRandomShape())
+            .addComponent(Position, getRandomPosition());
+
+        this.sessionIdToEntityMap[client.sessionId] = entity;
+
+        const velocity = entity.getComponent(Velocity);
+        console.log(
+            "INITIAL VELOCITY =>",
+            velocity.x,
+            velocity.y,
+            velocity.toJSON()
+        );
+
+        const shape = entity.getComponent(Shape);
+        console.log("INITIAL SHAPE =>", shape.primitive, shape.toJSON());
+
+        const position = entity.getComponent(Position);
+        console.log(
+            "INITIAL POSITION =>",
+            position.x,
+            position.y,
+            position.toJSON()
+        );
+    }
+
+    update(deltaTime: number) {
+        this.world.execute(deltaTime);
+    }
+
+    onLeave(client) {
+        const entity = this.sessionIdToEntityMap[client.sessionId];
+        const index = this.world.state.entities.indexOf(entity);
+        this.world.state.entities.splice(index, 1);
+        delete this.sessionIdToEntityMap[client.sessionId];
+    }
+}
+
+// Some helper functions when creating the components
+function getRandomVelocity() {
+    return {
+        x: SPEED_MULTIPLIER * (2 * Math.random() - 1),
+        y: SPEED_MULTIPLIER * (2 * Math.random() - 1),
+    };
+}
+
+function getRandomPosition() {
+    return {
+        x: Math.random() * canvasWidth,
+        y: Math.random() * canvasHeight,
+    };
+}
+
+function getRandomShape() {
+    return {
+        primitive: Math.random() >= 0.5 ? "circle" : "box",
+    };
+}
